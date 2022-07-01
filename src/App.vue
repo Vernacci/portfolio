3223fc289f90fa7a5e5d2295ad323fa8ca33ae9f
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-01 09:33:26 -0300

    Using components for better organization

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
-<script setup></script>
+<script setup>
+import Nav from './components/Nav.vue';
+</script>
 
 <template>
-  <nav class="menu">
-    <img src="" alt="" class="logo" />
-    <a href="Projects">Projects</a>
-    <a href="Contact">Contact me</a>
-  </nav>
-  <header class="intro">
-    <h1 class="name">Hi! I'm Victor Vernasqui 👋</h1>
-  </header>
+  <Nav />
+  <router-view></router-view>
 </template>
 
 <style>
@@ -16,24 +12,6 @@
   --black: #242424;
   --cambridge-blue: #96c5b0ff;
   --magic-mint: #adf1d2ff;
-}
-
-.menu {
-  display: flex;
-  margin-top: 10px;
-  justify-content: center;
-  gap: 30px;
-  border-bottom: 1px solid var(--cambridge-blue);
-  padding: 30px;
-}
-
-.menu a {
-  color: var(--black);
-  background-color: var(--cambridge-blue);
-  text-decoration: none;
-  font-size: 1.5rem;
-  padding: 10px;
-  border-radius: 8px;
 }
 
 .intro {
