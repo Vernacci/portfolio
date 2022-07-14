a855efcc3cb35f2f4e26030400055b1dddaff7f8
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-14 11:53:19 -0300

    added new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Projects from '../components/Projects.vue';
-import Header from '../components/Header.vue';
+import Home from '../components/Home.vue';
 import Contact from '../components/Contact.vue';
+import About from '../components/About.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Header,
+    component: Home,
   },
   {
     path: '/projects',
@@ -19,6 +20,11 @@ const routes = [
     path: '/contact',
     name: 'contact',
     component: Contact,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About,
   },
 ];
 
