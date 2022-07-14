be575d1955ca641a259d03d67b28e53ec85a731d
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-14 11:52:43 -0300

    Changed header for Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <h1>Hello i'm Victor Vernasqui 👋</h1>
+    <p>I'm a Front-end Developer based in Brazil</p>
+  </main>
+</template>
+
+<style>
+main {
+  margin: 60px auto;
+  margin-top: 120px;
+  max-width: 650px;
+  line-height: 1.9rem;
+  color: white;
+}
+
+h1 {
+  font-size: 2.5rem;
+}
+
+h1::after {
+  content: '';
+  width: 110px;
+  height: 2px;
+  margin-top: 10px;
+  display: block;
+  background-color: var(--green);
+}
+
+p {
+  font-size: 1.5rem;
+}
+</style>
