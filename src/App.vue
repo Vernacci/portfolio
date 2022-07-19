f6729838df138e2ac62d315a1048e9c3ea949a93
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-19 12:19:51 -0300

    removed styles from index.html

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,22 @@ import Nav from './components/Nav.vue';
   <router-view></router-view>
 </template>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap');
+
+:root {
+  --black: #0c0c0c;
+  --bg-black: #323232;
+  --yellow: #ffc83d;
+}
+
+body {
+  background-color: var(--black);
+  font-family: 'Rubik', sans-serif;
+}
+
+::selection {
+  background-color: var(--yellow);
+  color: var(--black);
+}
+</style>
