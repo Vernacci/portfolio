1cf6825a50b97bb7f43249a295341bf57c89b662
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-21 11:12:24 -0300

    Added projects structure

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,17 +1,47 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from 'vue';
+
+const projects = reactive([
+  {
+    name: 'Interactive Pricing Component',
+    img: '',
+    description: 'lorem ipsun',
+  },
+  { name: 'Advice Generator App', img: '', description: 'lorem ipsun' },
+  {
+    name: 'Tip Calculator App',
+    img: '',
+    description: 'lorem ipsun',
+  },
+  {
+    name: 'Interactive Pricing Component',
+    img: '',
+    description: 'lorem ipsun',
+  },
+]);
+</script>
 
 <template>
   <section>
     <h1>Projects</h1>
+    <div class="container" v-for="project in projects">
+      <h3>{{ project.name }}</h3>
+      <img src="../assets/calculator.png" alt="" />
+      <p>{{ project.description }}</p>
+    </div>
   </section>
 </template>
 
-<style>
+<style scoped>
 section {
   margin: 40px auto;
   margin-top: 120px;
   max-width: 650px;
   line-height: 1.9rem;
   color: white;
+}
+
+img {
+  width: 100%;
 }
 </style>
