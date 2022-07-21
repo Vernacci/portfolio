884bdb38052a85da3a80fb8c86919131abe4715a
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-21 12:30:32 -0300

    project page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,38 +1,52 @@
-<script setup>
-import { reactive, ref } from 'vue';
-
-const projects = reactive([
-  {
-    name: 'Interactive Pricing Component',
-    img: '',
-    description: 'lorem ipsun',
-  },
-  { name: 'Advice Generator App', img: '', description: 'lorem ipsun' },
-  {
-    name: 'Tip Calculator App',
-    img: '',
-    description: 'lorem ipsun',
-  },
-  {
-    name: 'Interactive Pricing Component',
-    img: '',
-    description: 'lorem ipsun',
-  },
-]);
-</script>
+<script setup></script>
 
 <template>
   <section>
     <h1>Projects</h1>
-    <div class="container" v-for="project in projects">
-      <h3>{{ project.name }}</h3>
-      <img src="../assets/calculator.png" alt="" />
-      <p>{{ project.description }}</p>
+    <div class="container">
+      <h3 class="title">Interactive rating Component</h3>
+      <img
+        src="./img/rating.png"
+        alt="Interactive Rating component screeshot"
+      />
+      <p class="description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+        dolorum dolores recusandae possimus tempora. Corrupti commodi obcaecati
+        unde id necessitatibus architecto esse distinctio nisi, minima
+        praesentium pariatur, eveniet nam saepe!
+      </p>
+      <h3 class="title">Advice Generator App</h3>
+      <img src="./img/advice.png" alt="Advice Generator App screeshot" />
+      <p class="description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+        dolorum dolores recusandae possimus tempora. Corrupti commodi obcaecati
+        unde id necessitatibus architecto esse distinctio nisi, minima
+        praesentium pariatur, eveniet nam saepe!
+      </p>
+      <h3 class="title">Tip Calculator App</h3>
+      <img src="./img/calculator.png" alt="Tip Calculator App screeshot" />
+      <p class="description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+        dolorum dolores recusandae possimus tempora. Corrupti commodi obcaecati
+        unde id necessitatibus architecto esse distinctio nisi, minima
+        praesentium pariatur, eveniet nam saepe!
+      </p>
+      <h3 class="title">Interactive Pricing Component</h3>
+      <img
+        src="./img/pricing.png"
+        alt="Interactive Pricing Component screeshot"
+      />
+      <p class="description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+        dolorum dolores recusandae possimus tempora. Corrupti commodi obcaecati
+        unde id necessitatibus architecto esse distinctio nisi, minima
+        praesentium pariatur, eveniet nam saepe!
+      </p>
     </div>
   </section>
 </template>
 
-<style scoped>
+<style>
 section {
   margin: 40px auto;
   margin-top: 120px;
