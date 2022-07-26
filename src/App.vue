25f2dbc935c719ae5630355677a494a94ec28d21
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-26 10:03:39 -0300

    made a custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import Nav from './components/Nav.vue';
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap');
 @import url('./bg.css');
-
 :root {
   --black: #0c0c0c;
   --bg-black: #323232;
@@ -26,4 +25,21 @@ body {
   background-color: var(--yellow);
   color: var(--black);
 }
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: var(--black);
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: var(--bg-black);
+}
+
+/* Handle on hover */
 </style>
