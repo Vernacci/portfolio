a9924bbb9025971ef8991ee4cab58a5da7876d60
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-26 10:03:51 -0300

    update contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,13 +3,22 @@
 <template>
   <section>
     <h1>Say hello!</h1>
+    <h2>You can find me here:</h2>
+
+    <a
+      href="mailto:victorvernasqui25@outlook.com"
+      class="mail"
+      rel="noopener noreferrer"
+      target="_blank"
+      >Send me a message!</a
+    >
   </section>
 </template>
 
 <style>
 section {
   margin: 60px auto;
-  margin-top: 100px;
+  margin-top: 70px;
   max-width: 650px;
   line-height: 1.9rem;
   color: var(--white);
