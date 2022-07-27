c2c0eb0a4ff2d06c369f6247e38947ce0384f6e1
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-27 10:16:31 -0300

    css update on email button

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,24 +3,45 @@
 <template>
   <section>
     <h1>Say hello!</h1>
-    <h2>You can find me here:</h2>
-
-    <a
-      href="mailto:victorvernasqui25@outlook.com"
-      class="mail"
-      rel="noopener noreferrer"
-      target="_blank"
-      >Send me a message!</a
-    >
+    <h2>You can call me here:</h2>
+    <div class="email-container">
+      <a
+        href="mailto:victorvernasqui25@outlook.com"
+        class="mail"
+        rel="noopener noreferrer"
+        target="_blank"
+        >Send me a message!</a
+      >
+    </div>
   </section>
 </template>
 
-<style>
+<style scoped>
 section {
   margin: 60px auto;
   margin-top: 70px;
   max-width: 650px;
   line-height: 1.9rem;
   color: var(--white);
+}
+
+.mail {
+  text-decoration: none;
+  color: var(--white);
+  background-color: var(--bg-black);
+  padding: 20px;
+  border-radius: 10px;
+  border: 2px solid transparent;
+  font-size: 1.3rem;
+  transition: all 0.5s;
+}
+
+.mail:hover {
+  background-color: transparent;
+  border: 2px solid var(--yellow);
+}
+
+.email-container {
+  margin-top: 50px;
 }
 </style>
