9a7897aa344b4e1415ec46af1f9d8f73ca00cd81
Victor Vernasqui <victorvernasqui12@gmail.com>
2022-07-29 10:08:52 -0300

    finished skills page

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,8 +1,32 @@
 <template>
   <section>
     <h1>Skills</h1>
-    <p>Work in progress 🙃</p>
+    <div class="skills-container">
+      <img src="./svgs/html.SVG" alt="HTML logo" />
+      <img src="./svgs/css.SVG" alt="css logo" />
+      <img src="./svgs/js.SVG" alt="js logo" />
+      <img src="./svgs/react.SVG" alt="react logo" />
+      <img src="./svgs/Vue.SVG" alt="Vue.js logo" />
+      <img src="./svgs/Git.SVG" alt="Git logo" />
+    </div>
   </section>
 </template>
 
-<style></style>
+<style scoped>
+.skills-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+}
+
+img {
+  width: 180px;
+  height: 180px;
+  filter: grayscale(100%);
+  transition: all 0.3s;
+}
+
+img:hover {
+  filter: none;
+}
+</style>
